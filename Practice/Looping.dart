0eeafef5a27fb2baca617c5e3a860211e6d1e7b7
mahin <mahincse47@gmail.com>
2024-02-19 23:25:17 +0600

    update

--- Practice/Looping.dart
+++ Practice/Looping.dart
@@ -1,5 +1,5 @@
 void main(){
-
+/*
   for(int i=0;i<=100;i++){
     if(i==0){
       continue;
@@ -8,5 +8,15 @@ void main(){
       break;
     }*/
     print('Lets Run the programme $i');
-  }
+  }*/
+
+List<String> studentList = ['mahin','nur','islam'];
+for(int i =0; i<studentList.length;i++){
+  print('Student name is $i ==> ${studentList[i]}');
+}
+
+//ForEach
+for(String item in studentList){
+  print('Student index $item');
+}
 }
