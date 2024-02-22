76ce0dcf420e6c3149cbb7d671cc4e2dd4e827bc
mahin <mahincse47@gmail.com>
2024-02-22 22:37:31 +0600

    update

--- Practice/function.dart
+++ Practice/function.dart
@@ -1,16 +1,27 @@
+import 'dart:io';
+
 main(){
   welComeMessage(name: 'Mahin');
   print(addNum(10, 10.11));
   double res =  addNum(20, 20.12);
   print(res.toStringAsFixed(2));
+  
+  print('Enter your name:');
+  String? data = stdin.readLineSync();
+  print(data);
+  myData(name: 'mahin',profession: 'Engineer/banker',location: 'Dhaka');
 }
 
-//reusibility
+//reducibility
 
 //function
 
 void welComeMessage({required String name,String address=''}){
   print('Hello Mr. $name, $address');
+}
+
+void myData({required String name,String profession='', String location=''}){
+  print('welcome to DART, Mr. $name, $profession, $location');
 }
 
 double addNum(int a, double b){
