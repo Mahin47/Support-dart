43842ae7de3e89119e10a24bb875fff038faa890
mahin <mahincse47@gmail.com>
2024-02-22 22:15:32 +0600

    update

--- Practice/function.dart
+++ Practice/function.dart
@@ -1,5 +1,5 @@
 main(){
-  welComeMessage('Mahin');
+  welComeMessage(name: 'Mahin');
   print(addNum(10, 10.11));
   double res =  addNum(20, 20.12);
   print(res.toStringAsFixed(2));
@@ -9,7 +9,7 @@ main(){
 
 //function
 
-void welComeMessage(String name,[String address='']){
+void welComeMessage({required String name,String address=''}){
   print('Hello Mr. $name, $address');
 }
 
