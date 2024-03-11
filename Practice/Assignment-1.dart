b50d9f8d40f832a3d33ed83f447413c034cf4107
mahin <mahincse47@gmail.com>
2024-03-12 04:54:21 +0600

    Assignment-1

--- Practice/Assignment-1.dart
+++ Practice/Assignment-1.dart
@@ -1,15 +1,23 @@
 class Media {
-
   play(){
     print('Playing media...');
   }
 }
 
+class Song extends Media {
+  String artist;
 
-
-
+  Song(this.artist);
+  
+  @override
+  play() {
+    print('Playing Song By $artist...');
+   // return super.play();
+  }
+}
 void main(){
-
   Media media = Media();
+  Song song = Song('The Artist name');
   media.play();
+  song.play();
 }
