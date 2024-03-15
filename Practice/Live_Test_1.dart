c395de6f5af0d5ace16c197e0f2f3491db3cad85
mahin <mahincse47@gmail.com>
2024-03-15 23:59:05 +0600

    Live_test_1

--- Practice/Live_Test_1.dart
+++ Practice/Live_Test_1.dart
@@ -4,8 +4,10 @@ void main() {
     {'name': 'Banana', 'color': 'Yellow', 'price': 1.0},
     {'name': 'Grapes', 'color': 'Purple', 'price': 3.0}
   ];
+  print('Original Fruit Details before Discount:');
 
   void displayFruitDetails(List<Map<String, dynamic>> fruits) {
+
     for (var fruit in fruits) {
       print('Name: ${fruit['name']}, Color: ${fruit['color']}, Price: ${fruit['price'].toStringAsFixed(2)}');
     }
@@ -20,7 +22,7 @@ void main() {
   displayFruitDetails(fruits);
 
   applyPriceDiscount(fruits, 10);
-  
+
   print('Fruit Details After Applying 10% Discount:');
   displayFruitDetails(fruits);
 }
